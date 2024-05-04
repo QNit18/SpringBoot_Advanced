cee9126b2fb92d444a3739eebd9c6332402246ff
QNit18 <vanquang1833@gmail.com>
2024-05-04 07:20:49 +0700

    Isolation Unit Test

--- src/test/java/com/qnit18/springadvanced/service/UserServiceTest.java
+++ src/test/java/com/qnit18/springadvanced/service/UserServiceTest.java
@@ -11,6 +11,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.TestPropertySource;
 
 import java.time.LocalDate;
 
@@ -22,6 +23,8 @@ import static org.mockito.Mockito.when;
 
 @SpringBootTest
 @Slf4j
+@TestPropertySource("/test.properties")
+
 public class UserServiceTest {
     @Autowired
     private UserService userService;
